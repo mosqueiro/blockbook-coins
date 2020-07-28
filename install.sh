34c64d0e03908a6a39909371135b760b80221754
Mosqueiro <erickcosta2010@gmail.com>
2020-07-27 22:41:21 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -71,10 +71,39 @@ sleep 1
 
 tee -a /etc/nginx/sites-enabled/default << END
 
+
+
 server {
         server_name ${domain};
-        location / {
+        location / {        
         proxy_pass https://localhost:9130;
+     if ($request_method = 'OPTIONS') {
+        add_header 'Access-Control-Allow-Origin' '*';
+        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+        #
+        # Custom headers and headers various browsers *should* be OK with but aren't
+        #
+        add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range';
+        #
+        # Tell client that this pre-flight info is valid for 20 days
+        #
+        add_header 'Access-Control-Max-Age' 1728000;
+        add_header 'Content-Type' 'text/plain; charset=utf-8';
+        add_header 'Content-Length' 0;
+        return 204;
+     }
+     if ($request_method = 'POST') {
+        add_header 'Access-Control-Allow-Origin' '*';
+        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+        add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range';
+        add_header 'Access-Control-Expose-Headers' 'Content-Length,Content-Range';
+     }
+     if ($request_method = 'GET') {
+        add_header 'Access-Control-Allow-Origin' '*';
+        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+        add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range';
+        add_header 'Access-Control-Expose-Headers' 'Content-Length,Content-Range';
+     }        
     }
 
 
@@ -86,14 +115,16 @@ server {
 
 }
 server {
+
         server_name ${domain};
         location / {
         proxy_pass https://localhost:9130;
     }
 
-
     listen 80;
+    return 301 https://$host$request_uri;
 }
+
 
 END
 
