8b78f32fa3e562e38b470b82cdaf968a9c0e0a6c
Mosqueiro <erickcosta2010@gmail.com>
2022-03-30 18:09:25 -0300

    Update text.go

--- api/text.go
+++ api/text.go
@@ -1,21 +1,10 @@
 package api
 
-import (
-	"strings"
-
-	"github.com/gobuffalo/packr"
-)
-
 // Text contains static overridable texts used in explorer
 var Text struct {
 	BlockbookAbout, TOSLink string
 }
 
 func init() {
-	box := packr.NewBox("../build/text")
-	if about, err := box.MustString("about"); err == nil {
-		Text.BlockbookAbout = strings.TrimSpace(about)
-	} else {
-		panic(err)
-	}
+	Text.BlockbookAbout = "Blockbook - blockchain indexer for Trezor wallet https://trezor.io/. Do not use for any other purpose."
 }
